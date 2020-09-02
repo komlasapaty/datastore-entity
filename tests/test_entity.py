613a67b738f2a1e191de692a305bb7dfc5eee0dd
Seyram Komla <komlasapaty@gmail.com>
2020-09-02 15:48:23 +0000

    add test for EntityValue

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -86,3 +86,18 @@ class TestEntity:
         assert entity.connect() == True
     
 
+class TestEntityValue:
+    def test_none_value(self):
+        """
+        Check that when EntityValue is initialized without a value, it defaults to `None`
+        """
+        entity_value = EntityValue()
+        assert entity_value.value is None
+    
+    def test_value_passed(self):
+        """
+        Check that the value passed to EntityValue is what is returned
+        """
+        value = "foobar" 
+        entity_value = EntityValue(value)
+        assert entity_value.value == value
