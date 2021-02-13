952c5227d0138d1760d87aa25be06a2a12a01a65
Seyram Komla <komlasapaty@gmail.com>
2021-02-13 22:00:42 +0000

    sync versin number in __about__.py

--- datastore_entity/__about__.py
+++ datastore_entity/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'Datastore-Entity'
 __description__ = 'An ORM-Like interface for Google Cloud NoSQL Datastore'
 __url__ = 'https://github.com/komlasapaty/datastore-entity'
-__version_info__ = ('0', '1', '0')
+__version_info__ = ('0', '2', '0')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Seyram Komla Sapaty'
 __author_email__ = 'komlasapaty@gmail.com'
